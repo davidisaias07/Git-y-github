4df09cd4a5b529cc6bf7802aaaf0ed7dda156962
David Perez <davidisaias4465365@gmail.com>
2022-03-13 10:02:21 -0500

    Se corrigió codigo redundante en el menu.py

--- menu.py
+++ menu.py
@@ -3,18 +3,6 @@ def menu (tipo_plato,precio):
     print("Elegiste " + tipo_plato+", el precio de: " + str(precio)+ " soles")
 
 
-platos_comida = """ Bievenido al menú, te presentamos nuestros platos disponibles:
-
-<<<<<<< HEAD
-1- Arroz con carne
-=======
-1- Arroz con papas
->>>>>>> v0.1
-2- Fideos al alfredo
-3- Tallarines rojos con carne
-4- Plato oculto
-
-"""
 
 platos_comida = int(input("""Bievenido al menú, te presentamos nuestros platos disponibles:
 
@@ -26,11 +14,7 @@ Elije una opción: """))
 
 
 if platos_comida == 1:
-<<<<<<< HEAD
     menu("arroz con carne", 14)
-=======
-    menu("arroz con papas", 14)
->>>>>>> v0.1
 
 elif platos_comida == 2:
     menu("fideos al alfredo", 17)
