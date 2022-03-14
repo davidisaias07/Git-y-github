11cb30efe0683c6abb69fb93faaebbcf7878e244
David Perez <davidisaias4465365@gmail.com>
2022-03-13 21:47:41 -0500

    Menú sin plato secreto

--- menu.py
+++ menu.py
@@ -9,7 +9,6 @@ platos_comida = int(input("""Bievenido al menú, te presentamos nuestros platos 
 1- Arroz con pollo
 2- Fideos al alfredo
 3- Tallarines rojos con carne
-4- Plato oculto
 Elije una opción: """))
 
 
@@ -21,9 +20,6 @@ elif platos_comida == 2:
 
 elif platos_comida == 3:
     menu("tallarines rojos con carne", 12.5)
-
-elif platos_comida == 4:
-    print("Plato oculto está disponible solo los viernes")
 
 else:
     while platos_comida > 4:
